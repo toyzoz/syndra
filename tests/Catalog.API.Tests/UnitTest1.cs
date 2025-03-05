5ffff0ed01c2e6c37b15c70d938a71d5eb4e7256
galio <yangkun>
2025-03-06 01:53:12 +0800

    修改 UnitTest1.cs 中的测试变量值

--- tests/Catalog.API.Tests/UnitTest1.cs
+++ tests/Catalog.API.Tests/UnitTest1.cs
@@ -5,8 +5,8 @@
         [Fact]
         public void Test1()
         {
-            var a = 1;
-            var b = 2;
+            var a = 1.0;
+            var b = 1;
 
             Assert.Equal(a, b);
         }
